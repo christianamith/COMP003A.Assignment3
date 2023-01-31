b3fc74cb0bcb269a8519336fe5732ca84fa75a99
Christian Amith <christianamith@my.whccd.edu>
2023-01-30 21:05:07 -0800

    Add project files.

---
+++ COMP003A.Assignment3/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Author:      Christian Amith 
+ * Course:      COMP-003A
+ * Purpose:     Code for 3rd week assignment
+ */
+
+namespace COMP003A.Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numeric to letter grade using an if-else statement");
+            Console.Write("Enter a numeric grade (0-100): ");
+            string numGradeText = Console.ReadLine();
+            int numericGrade = Convert.ToInt32(numGradeText);
+
+            // If-Else Section (Numeric to Letter Grade)
+            if (numericGrade >= 0 && numericGrade <= 100)
+            {
+                Console.ForegroundColor= ConsoleColor.White;
+
+                if (numericGrade >= 90)
+                    Console.WriteLine("Letter Grade: A+");
+                else if (numericGrade >= 80)
+                    Console.WriteLine("Letter Grade: B");
+                else if (numericGrade >= 70)
+                    Console.WriteLine("Letter Grade: C");
+                else if (numericGrade >= 60)
+                    Console.WriteLine("Letter Grade: D");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+            Console.WriteLine("*************************************************");
+        }
+    }
+}
